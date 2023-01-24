f956eb6482a8abd0ff9c6df9d9f6d916e3d690ed
dogukantayuglu <dogukantayuglu@gmail.com>
2023-01-24 20:19:46 +0300

    refactor player controller

--- bubble-pops/Assets/Game/Scripts/Controllers/PlayerController.cs
+++ bubble-pops/Assets/Game/Scripts/Controllers/PlayerController.cs
@@ -1,37 +1,26 @@
-using System.Collections.Generic;
-using Game.Scripts.Bubble;
 using Game.Scripts.Interfaces;
+using Game.Scripts.Player;
 using UnityEngine;
 
 namespace Game.Scripts.Controllers
 {
     public class PlayerController : MonoBehaviour
     {
-        public IBubbleBuffer BubbleBuffer { get; set; }
-        
-        [SerializeField] private Transform queueStartTransform;
-        [SerializeField] private float horizontalQueueSpace = 0.5f;
-        [SerializeField] private int maxQueueCount = 2;
+        public IBubbleBuffer BubbleBuffer
+        {
+            set => playerBubbleHandler.BubbleBuffer = value;
+        }
 
-        private List<BubbleEntity> _bubblesInQueue;
+        [SerializeField] private PlayerBubbleHandler playerBubbleHandler;
 
         public void Initialize()
         {
-            _bubblesInQueue = new List<BubbleEntity>();
+            playerBubbleHandler.Initialize();
         }
-
+        
         public void ActivateInitBubbles()
         {
-            for (var i = 0; i < maxQueueCount; i++)
-            {
-                var bubbleEntity = BubbleBuffer.GetBubbleForPlayer();
-                var position = queueStartTransform.position;
-                position.x -= i * horizontalQueueSpace;
-                queueStartTransform.position = position;
-                _bubblesInQueue.Add(bubbleEntity);
-                bubbleEntity.ActivateAtQueue(position, i != 0);
-            }
+            playerBubbleHandler.ActivateInitBubbles();
         }
-
     }
 }
---
+++ bubble-pops/Assets/Game/Scripts/Player/PlayerBubbleHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using Game.Scripts.Bubble;
+using Game.Scripts.Interfaces;
+using UnityEngine;
+
+namespace Game.Scripts.Player
+{
+    public class PlayerBubbleHandler : MonoBehaviour
+    {
+        public IBubbleBuffer BubbleBuffer { get; set; }
+        
+        [SerializeField] private Transform queueStartTransform;
+        [SerializeField] private float horizontalQueueSpace = 0.5f;
+        [SerializeField] private int maxQueueCount = 2;
+
+        private List<BubbleEntity> _bubblesInQueue;
+        
+        public void Initialize()
+        {
+            _bubblesInQueue = new List<BubbleEntity>();
+        }
+
+        public void ActivateInitBubbles()
+        {
+            for (var i = 0; i < maxQueueCount; i++)
+            {
+                var bubbleEntity = BubbleBuffer.GetBubbleForPlayer();
+                var position = queueStartTransform.position;
+                position.x -= i * horizontalQueueSpace;
+                queueStartTransform.position = position;
+                _bubblesInQueue.Add(bubbleEntity);
+                bubbleEntity.ActivateAtQueue(position, i != 0);
+            }
+        }
+    }
+}
